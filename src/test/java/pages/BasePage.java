8e6ef0d2ece40bd4730a805ed8ab9150b53de06a
Yusuf Kart <mkagan211@gmail.com>
2026-09-21 05:10:40 +0300

    Use keyboard activation for headless Chrome

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,9 +2,9 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -33,10 +33,7 @@ public class BasePage {
 
     protected void click(WebElement element) {
         WebElement clickableElement = wait.until(ExpectedConditions.elementToBeClickable(element));
-        new Actions(driver)
-                .moveToElement(clickableElement)
-                .click()
-                .perform();
+        clickableElement.sendKeys(Keys.ENTER);
     }
 
     protected void type(WebElement element, String text) {
